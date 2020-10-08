51ad9b67f663edac66713b3b22458cc53c46a150
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-10-08 18:13:56 +0200

    Add basic userscontroller

---
+++ Vecto.Api/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using Vecto.Core.Entities;
+using Vecto.Core.Interfaces;
+
+namespace Vecto.Api.Controllers
+{
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IRepository<User> _userRepository;
+        private readonly IConfiguration _configuration;
+
+
+        public UsersController(IRepository<User> userRepository, IConfiguration config)
+        {
+            _configuration = config;
+            _userRepository = userRepository;
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var user = _userRepository.GetBy(id);
+            return user == null ? (IActionResult)NotFound() : Ok(user);
+        }
+    }
+}
