a6fa844a2b3a95c0f7d3d7cf06731a37791febe1
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-10-23 15:32:14 +0200

    Fix namespaces

--- Vecto.UWP/Pages/Authentication/LoginPage.xaml.cs
+++ Vecto.UWP/Pages/Authentication/LoginPage.xaml.cs
@@ -1,26 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
 
-namespace Vecto.UWP
+namespace Vecto.UWP.Pages.Authentication
 {
     public sealed partial class LoginPage : Page
     {
         public LoginPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
