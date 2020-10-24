07ddd1f909b9d8f8fd54b779b04193845091ceb6
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-10-24 13:42:14 +0200

    Add way to use an appsettings.json
     using singleton class

---
+++ Vecto.UWP/AppSettings.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Vecto.UWP
+{
+    /// <summary>
+    /// Singleton class 
+    /// </summary>
+    public class AppSettings
+    {
+        public static AppSettings Instance { get; } = new AppSettings();
+
+        public IConfiguration Configuration { get; protected set; }
+
+        private AppSettings()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = builder.Build();
+        }
+
+        public static string GetSectionString(string key) =>
+            Instance.Configuration.GetSection(key).Value;
+    }
+}
