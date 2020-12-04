67b8b49b9dac53523412f0f94a154e1cb69dd082
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-12-04 23:09:23 +0100

    Implement getting a trip by id

--- Vecto.Api/Controllers/TripsController.cs
+++ Vecto.Api/Controllers/TripsController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentValidation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Vecto.Application.Trips;
+using Vecto.Core.Entities;
 using Vecto.Core.Interfaces;
 
 namespace Vecto.Api.Controllers
@@ -26,13 +28,24 @@ namespace Vecto.Api.Controllers
         }
         
         [HttpGet("")]
-        public ActionResult<IList<TripDTO>> Get()
+        public ActionResult<IList<Trip>> GetAll()
         {
             var user = _userRepository.GetBy(User.Identity.Name);
             return Ok(user.Trips);
         }
         
- 
+        [HttpGet("{id}")]
+        public ActionResult<IList<Trip>> GetBy(Guid id)
+        {
+            var user = _userRepository.GetBy(User.Identity.Name);
+            if (user == null) return BadRequest();
+
+            var trip = user.Trips.SingleOrDefault(t => t.Id.Equals(id));
+            if (trip == null) return BadRequest();
+            
+            return Ok(trip);
+        }
+
         [HttpPost("")]
         public ActionResult<IList<TripDTO>> Add([FromBody]TripDTO model)
         {
@@ -40,7 +53,7 @@ namespace Vecto.Api.Controllers
             user.Trips.Add(model.MapToTrip());
             _userRepository.Update(user);
             _userRepository.SaveChanges();
-            return RedirectToAction("Get");
+            return RedirectToAction("GetAll");
         }
         
     }
