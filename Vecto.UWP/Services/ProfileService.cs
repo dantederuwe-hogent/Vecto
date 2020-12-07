513736ff4542a8390fa5f1f8d97ec39ab43576c3
Liam Spitaels <liam.spitaels00@gmail.com>
2020-12-07 17:36:29 +0100

    Add ProfileService

---
+++ Vecto.UWP/Services/ProfileService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Vecto.Application.Profile;
+using Windows.Storage;
+
+namespace Vecto.UWP.Services
+{
+    public class ProfileService
+    {
+        private readonly HttpClient _client;
+        private string token;
+
+        public ProfileService()
+        {
+            _client = new HttpClient()
+            {
+                BaseAddress = new Uri(AppSettings.GetSectionString("ApiBaseUrl"))
+            };
+            token = ApplicationData.Current.LocalSettings.Values["token"].ToString();
+            _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+        }
+
+        public async Task<ProfileDTO> GetProfile()
+        {
+            var result = await _client.GetAsync("profile");
+            return JsonConvert.DeserializeObject<ProfileDTO>(await result.Content.ReadAsStringAsync());
+        }
+    }
+}
