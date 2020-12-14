97de97a27755d7f6308d6a3530bf6cf7cb0cae90
Liam Spitaels <liam.spitaels00@gmail.com>
2020-12-14 21:39:40 +0100

    Display trips in a more pretty manner

---
+++ Vecto.UWP/Converters/DateTimeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace Vecto.UWP.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value != null) return ((DateTime)value).ToString((string)parameter);
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
