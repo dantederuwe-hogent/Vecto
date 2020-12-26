97465f363202bee0f59d09adaad9a5c0c79e8ad9
Liam Spitaels <liam.spitaels00@gmail.com>
2020-12-26 16:42:37 +0100

    Add log out functionality

--- Vecto.UWP/Pages/ProfilePage.xaml.cs
+++ Vecto.UWP/Pages/ProfilePage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Vecto.UWP.Pages.Authentication;
+using Windows.Security.Credentials;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Vecto.UWP.Pages
 {
@@ -7,6 +11,23 @@ namespace Vecto.UWP.Pages
         public ProfilePage()
         {
             this.InitializeComponent();
+        }
+
+        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            // clear token
+            var localSettings = ApplicationData.Current.LocalSettings;
+            localSettings.Values.Remove("token");
+
+            // clear password
+            var passwordVault = new PasswordVault();
+            var credentialEnumerator = passwordVault.FindAllByResource("Vecto").GetEnumerator();
+            credentialEnumerator.MoveNext();
+            passwordVault.Remove(credentialEnumerator.Current);
+
+            // get root frame and use that to navigate otherwise we navigate within the nav menu
+            var parentFrame = Window.Current.Content as Frame;
+            parentFrame.Navigate(typeof(LoginPage));
         }
     }
 }
